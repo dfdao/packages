ab16c4a28c85a02dca20590d7e71e33f36257345
Henry Caron <henryacaron@gmail.com>
2022-05-05 10:40:24 -0400

    feat: arena leaderboard types

--- types/src/database_types.ts
+++ types/src/database_types.ts
@@ -25,3 +25,15 @@ export interface LeaderboardEntry {
   ethAddress: EthAddress;
   twitter?: string;
 }
+
+export interface ArenaLeaderboard {
+  entries: ArenaLeaderboardEntry[];
+}
+
+export interface ArenaLeaderboardEntry {
+  ethAddress: EthAddress;
+  twitter?: string;
+  games: number;
+  wins: number;
+}
+
