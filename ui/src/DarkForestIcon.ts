09a4758a15ad4ead109a5a3ac37e23fc444ba0c3
Henry Caron <henryacaron@gmail.com>
2022-06-16 17:56:40 -0400

    feat: updated styles and procgen config

--- ui/src/DarkForestIcon.ts
+++ ui/src/DarkForestIcon.ts
@@ -1,6 +1,6 @@
 import { Abstract } from '@darkforest_eth/types';
 import { css, LitElement, nothing, svg, unsafeCSS } from 'lit';
-import * as dfstyles from './styles';
+import dfstyles from './styles';
 
 export type IconType = Abstract<string, 'IconType'>;
 
