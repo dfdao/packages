b575b23bb0fc7c5af0853e0d251a64a3fb2bfeca
Henry Caron <henryacaron@gmail.com>
2022-06-27 10:43:06 -0400

    feat: new claim victory typing

--- types/src/transactions.ts
+++ types/src/transactions.ts
@@ -27,7 +27,7 @@ export type ContractMethodName =
   | 'createLobby'
   | 'invadePlanet'
   | 'capturePlanet'
-  | "claimTargetPlanetVictory"
+  | "claimVictory"
   | "createPlanet"
   | "createArenaPlanet";
 
@@ -226,8 +226,7 @@ export type UnconfirmedCapturePlanet = TxIntent & {
  * @hidden
  */
  export declare type UnconfirmedClaimVictory = TxIntent & {
-  methodName: 'claimTargetPlanetVictory';
-  locationId: LocationId;
+  methodName: 'claimVictory';
 };
 /**
  * @hidden
