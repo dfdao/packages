28466d134480bd3142437739fcfb91f834c8704c
cha0sg0d <0xcha0sg0d@gmail.com>
2022-07-11 23:01:35 -0600

    feat: convert graph types to BigInt

--- types/src/graph_types.ts
+++ types/src/graph_types.ts
@@ -5,78 +5,76 @@ import {
   Tuple6,
 } from '@darkforest_eth/settings/dist/decoder-helpers';
 
+import { BigNumber } from 'ethers';
+
 export interface GraphConfig {
-  // START_PAUSED,
-  ADMIN_CAN_ADD_PLANETS: boolean;
-  TOKEN_MINT_END_TIMESTAMP: number;
-  WORLD_RADIUS_LOCKED: boolean;
-  WORLD_RADIUS_MIN: number;
-  DISABLE_ZK_CHECKS: boolean;
-  PLANETHASH_KEY: number;
-  SPACETYPE_KEY: number;
-  BIOMEBASE_KEY: number;
-  PERLIN_MIRROR_X: boolean;
-  PERLIN_MIRROR_Y: boolean;
-  PERLIN_LENGTH_SCALE: number;
-  MAX_NATURAL_PLANET_LEVEL: number;
-  TIME_FACTOR_HUNDREDTHS: number;
-  PERLIN_THRESHOLD_1: number;
-  PERLIN_THRESHOLD_2: number;
-  PERLIN_THRESHOLD_3: number;
-  INIT_PERLIN_MAX: number;
-  INIT_PERLIN_MIN: number;
-  BIOME_THRESHOLD_1: number;
-  BIOME_THRESHOLD_2: number;
-  PLANET_LEVEL_THRESHOLDS: ExactArray10<number>;
-  PLANET_RARITY: number;
-  PLANET_TRANSFER_ENABLED: boolean;
-  PHOTOID_ACTIVATION_DELAY: number;
-  SPAWN_RIM_AREA: number;
-  LOCATION_REVEAL_COOLDOWN: number;
   // # CLAIM_PLANET_COOLDOWN,
-  // # PLANET_TYPE_WEIGHTS,
-  SILVER_SCORE_VALUE: number;
-  ARTIFACT_POINT_VALUES: Tuple6<number>;
-  SPACE_JUNK_ENABLED: boolean;
-  SPACE_JUNK_LIMIT: number;
-  PLANET_LEVEL_JUNK: ExactArray10<number>;
-  ABANDON_SPEED_CHANGE_PERCENT: number;
-  ABANDON_RANGE_CHANGE_PERCENT: number;
-  CAPTURE_ZONES_ENABLED: boolean;
-  CAPTURE_ZONE_COUNT: number;
-  CAPTURE_ZONE_CHANGE_BLOCK_INTERVAL: number;
-  CAPTURE_ZONE_RADIUS: number;
-  CAPTURE_ZONE_PLANET_LEVEL_SCORE: ExactArray10<number>;
-  CAPTURE_ZONE_HOLD_BLOCKS_REQUIRED: number;
-  CAPTURE_ZONES_PER_5000_WORLD_RADIUS: number;
-  MANUAL_SPAWN: boolean;
-  TARGET_PLANETS: boolean;
-  CLAIM_VICTORY_ENERGY_PERCENT: number;
-  MODIFIERS: ExactArray8<number>;
-  SPACESHIPS: ExactArray5<boolean>;
-  // # RANDOM_ARTIFACTS,
-  NO_ADMIN: boolean;
-  // # INIT_PLANETS,
-  CONFIRM_START: boolean;
-  TARGETS_REQUIRED_FOR_VICTORY: number;
+  ABANDON_RANGE_CHANGE_PERCENT: BigNumber;
+  ABANDON_SPEED_CHANGE_PERCENT: BigNumber;
+  ADMIN_CAN_ADD_PLANETS: boolean;
+  ARTIFACT_POINT_VALUES: Tuple6<BigNumber>;
+  BIOME_THRESHOLD_1: BigNumber;
+  BIOME_THRESHOLD_2: BigNumber;
+  BIOMEBASE_KEY: BigNumber;
   BLOCK_CAPTURE: boolean;
   BLOCK_MOVES: boolean;
-  TEAMS_ENABLED: boolean;
-  NUM_TEAMS: number;
-  RANKED: boolean;
-  WHITELIST_ENABLED: boolean;
-  RANDOM_ARTIFACTS: boolean;
-  START_PAUSED: boolean;
+  CAPTURE_ZONE_CHANGE_BLOCK_INTERVAL: BigNumber;
+  CAPTURE_ZONE_COUNT: BigNumber;
+  CAPTURE_ZONE_HOLD_BLOCKS_REQUIRED: BigNumber;
+  CAPTURE_ZONE_PLANET_LEVEL_SCORE: ExactArray10<BigNumber>;
+  CAPTURE_ZONE_RADIUS: BigNumber;
+  CAPTURE_ZONES_ENABLED: boolean;
+  CAPTURE_ZONES_PER_5000_WORLD_RADIUS: BigNumber;
+  CLAIM_VICTORY_ENERGY_PERCENT: BigNumber;
+  CONFIRM_START: boolean;
+  DISABLE_ZK_CHECKS: boolean;
+  INIT_PERLIN_MAX: BigNumber;
+  INIT_PERLIN_MIN: BigNumber;
+  LOCATION_REVEAL_COOLDOWN: BigNumber;
+  MANUAL_SPAWN: boolean;
+  MAX_NATURAL_PLANET_LEVEL: BigNumber;
+  MODIFIERS: ExactArray8<BigNumber>;
+  NO_ADMIN: boolean;
+  NUM_TEAMS: BigNumber;
+  PERLIN_LENGTH_SCALE: BigNumber;
+  PERLIN_MIRROR_X: boolean;
+  PERLIN_MIRROR_Y: boolean;
+  PERLIN_THRESHOLD_1: BigNumber;
+  PERLIN_THRESHOLD_2: BigNumber;
+  PERLIN_THRESHOLD_3: BigNumber;
+  PHOTOID_ACTIVATION_DELAY: BigNumber;
+  PLANET_LEVEL_JUNK: ExactArray10<BigNumber>;
+  PLANET_LEVEL_THRESHOLDS: ExactArray10<BigNumber>;
+  PLANET_RARITY: BigNumber;
+  PLANET_TRANSFER_ENABLED: boolean;
   PLANET_TYPE_WEIGHTS: number[];
+  PLANETHASH_KEY: BigNumber;
+  RANDOM_ARTIFACTS: boolean;
+  RANKED: boolean;
+  SILVER_SCORE_VALUE: BigNumber;
+  SPACE_JUNK_ENABLED: boolean;
+  SPACE_JUNK_LIMIT: BigNumber;
+  SPACESHIPS: ExactArray5<boolean>;
+  SPACETYPE_KEY: BigNumber;
+  SPAWN_RIM_AREA: BigNumber;
+  START_PAUSED: boolean;
+  TARGET_PLANETS: boolean;
+  TARGETS_REQUIRED_FOR_VICTORY: BigNumber;
+  TEAMS_ENABLED: boolean;
+  TIME_FACTOR_HUNDREDTHS: BigNumber;
+  TOKEN_MINT_END_TIMESTAMP: BigNumber;
+  WHITELIST_ENABLED: boolean;
+  WORLD_RADIUS_LOCKED: boolean;
+  WORLD_RADIUS_MIN: BigNumber;
 }
 
 export interface GraphPlanet {
   x: number;
   y: number;
-  level: number;
-  planetType: "PLANET" | "SILVER_MINE" | "RUINS" | "TRADING_POST" | "SILVER_BANK";
+  level: BigNumber;
+  planetType: 'PLANET' | 'ASTEROID' | 'FOUNDRY' | 'SPACETIME_RIP' | 'QUASAR';
   locationDec: string;
-  perlin: number;
+  perlin: BigNumber;
   targetPlanet: boolean;
   spawnPlanet: boolean;
 }
@@ -85,7 +83,7 @@ export interface RawAccount {
   wins: number;
   matches: number;
   arenaPlayers: {
-    arena: GraphArena
+    arena: GraphArena;
   }[];
 }
 
